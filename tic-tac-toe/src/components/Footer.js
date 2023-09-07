d2514d2fc4a76088fd6c4f9332d9700e22488585
Bourgain-N <natacha.bourgain.nb@gmail.com>
2023-09-07 12:37:43 +0200

    professeur sarah

--- tic-tac-toe/src/components/Footer.js
+++ tic-tac-toe/src/components/Footer.js
@@ -3,7 +3,7 @@
 const Footer = () =>{
     return (
         <footer className="">
-            <p className=" display playerX">La meilleur application créée par Natacha et Sarah</p>
+            <p className=" display playerX">La meilleur application créée par Natacha et Sarah</p> 
         </footer>
     );
 };
