91f05d734590b07082db48dfb1ad071ceea13b25
SarahZwahlen <sarah.zwahlen@wanadoo.fr>
2023-09-07 11:10:56 +0200

    fix board game style

--- tic-tac-toe/src/components/Square.js
+++ tic-tac-toe/src/components/Square.js
@@ -3,15 +3,21 @@ import { useState } from "react";
 const Square = ({ value, squareIsClicked, numero }) => {
   const [isClicked, setIsClicked] = useState(false);
   const [symbol, setSymbol] = useState(null);
+  const [isX, setisX] = useState(null);
 
   const handleClick = () => {
     setIsClicked(true);
     setSymbol(value);
     squareIsClicked(numero);
+    value === "X" ? setisX("playerX") : setisX("playerO");
   };
 
   return (
-    <button disabled={isClicked} className="case" onClick={handleClick}>
+    <button
+      disabled={isClicked}
+      className={`case ${isX}`}
+      onClick={handleClick}
+    >
       {isClicked && <span>{symbol}</span>}
     </button>
   );
