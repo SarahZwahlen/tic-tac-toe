9d645b0d1cf4639338d95b5c193dc387286e89be
SarahZwahlen <sarah.zwahlen@wanadoo.fr>
2023-09-07 12:48:45 +0200

    delete current game data

--- tic-tac-toe/src/components/NavBar.js
+++ tic-tac-toe/src/components/NavBar.js
@@ -1,23 +1,29 @@
-
-
-const NavBar = ({handleNewGame}) =>{
- 
-    return (
-        <header className="background">
-        <h1 className="title">
-            Super Morpion
-        </h1>
-        <img src=""></img>
-        <nav>
-            <ul className="controls">
-                <li>
-                    <button className="reset" onClick={handleNewGame}>
-                        Rejouer
-                    </button>
-                </li>
-            </ul>
-        </nav>
-        </header>
-    );
+const NavBar = ({ handleNewGame }) => {
+  return (
+    <header className="background">
+      <h1 className="title">Super Morpion</h1>
+      <img src=""></img>
+      <nav>
+        <ul className="controls">
+          <li>
+            <button className="reset" onClick={handleNewGame}>
+              Rejouer
+            </button>
+          </li>
+          <li>
+            <button
+              className="reset"
+              onClick={() => {
+                localStorage.removeItem("player1");
+                localStorage.removeItem("player2");
+              }}
+            >
+              Recommencer la partie
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 export default NavBar;
