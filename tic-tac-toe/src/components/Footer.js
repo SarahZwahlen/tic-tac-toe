09311a735dc0f3fa4febdb86edd45b162fe81290
SarahZwahlen <sarah.zwahlen@wanadoo.fr>
2023-09-07 16:18:47 +0200

    make super top style

--- tic-tac-toe/src/components/Footer.js
+++ tic-tac-toe/src/components/Footer.js
@@ -4,29 +4,40 @@ const Footer = () => {
   const natachaData = {
     name: "Natacha Bourgain",
     ppUrl: "https://avatars.githubusercontent.com/u/126149427?v=4",
+    repoUrl: "https://github.com/Bourgain-N",
+    githubTag: "Bourgain-N",
+    linkedIn: "https://www.linkedin.com/in/natacha-bourgain-a12914264/",
   };
 
   const sarahData = {
     name: "Sarah Zwahlen",
     ppUrl: "https://avatars.githubusercontent.com/u/122228917?v=4",
+    repoUrl: "https://github.com/SarahZwahlen",
+    githubTag: "SarahZwahlen",
+    linkedIn: "https://www.linkedin.com/in/sarah-zwahlen-8bb858156/",
   };
 
   return (
     <footer className="footer">
-       <div className="waves">
+      <div className="waves">
         <div className="wave wave1"></div>
         <div className="wave wave2"></div>
         <div className="wave wave3"></div>
         <div className="wave wave4"></div>
       </div>
-      <p className=" display playerX">
-        La meilleure application créée par Natacha et Sarah
-      </p>
-      <DevCard devData={natachaData} />
-      <p className=" display2">Merci Professeur Sarah</p>
-      <p className=" display2">T'es un super padawan Natacha</p>
-
-      <DevCard devData={sarahData} />
+      <div className="credits">
+        <p className=" display playerX">
+          La meilleure application créée par Natacha et Sarah
+        </p>
+        <div className="greatings">
+          <DevCard devData={natachaData} />
+          <div className="messages">
+            <p className=" display2">Merci Professeur Sarah</p>
+            <p className=" display2">T'es un super padawan Natacha</p>
+          </div>
+          <DevCard devData={sarahData} />
+        </div>
+      </div>
     </footer>
   );
 };
--- tic-tac-toe/src/components/devCard.js
+++ tic-tac-toe/src/components/devCard.js
@@ -1,8 +1,20 @@
+import githubLogo from "../assets/media/octo-cat.png";
+import linkedinLogo from "../assets/media/linkedin-white.png";
 const DevCard = ({ devData }) => {
   return (
     <div className="dev-card">
-      <h3>{devData.name}</h3>
-      <img src={devData.ppUrl} />
+      <img className="dev-pp" src={devData.ppUrl} />
+      <div className="dev-infos">
+        <h3>{devData.name}</h3>
+        <div className="dev-links">
+          <img src={githubLogo} className="logo" />
+          <a href={`${devData.repoUrl}`}>@{devData.githubTag}</a>
+        </div>
+        <div className="dev-links">
+          <img src={linkedinLogo} className="logo" />
+          <a href={`${devData.linkedIn}`}>Par ici</a>
+        </div>
+      </div>
     </div>
   );
 };
