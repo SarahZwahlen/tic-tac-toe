3b8a3f9eba69f5eac9e0f352051724a6d67d9fce
Bourgain-N <natacha.bourgain.nb@gmail.com>
2023-09-07 16:52:00 +0200

    super wave

--- tic-tac-toe/src/components/NavBar.js
+++ tic-tac-toe/src/components/NavBar.js
@@ -1,6 +1,13 @@
 const NavBar = ({ handleNewGame }) => {
   return (
+    
     <header className="background">
+      <div className="waves-rotate">
+          <div className="wave wave1"></div>
+          <div className="wave wave2"></div>
+          <div className="wave wave3"></div>
+          <div className="wave wave4"></div>
+      </div>
       <h1 className="title">Super Morpion</h1>
       <nav>
         <ul className="controls">
